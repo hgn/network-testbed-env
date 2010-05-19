d27a57a1d6c8da46d045367cf010dde556400a56
Hagen Paul Pfeifer <hagen@jauu.net>
2010-05-20 00:15:49 +0200

    generate 3 images

--- gen-qemu-images.sh
+++ gen-qemu-images.sh
@@ -5,9 +5,18 @@ losetup /dev/loop0 diskimage.img
 mke2fs /dev/loop0
 mkdir mounted
 mount /dev/loop0 mounted
-debootstrap sid mounted/ http://ftp.us.debian.org/debian/
+debootstrap sid mounted/ http://ftp.de.debian.org/debian/
 chown pfeifer diskimage.img
 
+# install software
+
+# clone raw images
+mv diskimage.img alpha.img
+cp alpha.img bravo.img
+cp alpha.img charlie.img
+
+# cleanup
 umount mounted
 losetup -d /dev/loop0
+rm -a mounted
 
