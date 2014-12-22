ea2f9b82960c9a986d89840676a1adc77ab434a4
Hagen Paul Pfeifer <hagen@jauu.net>
2014-12-22 22:11:48 +0100

    some fixes
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- launch-testbed.sh
+++ launch-testbed.sh
@@ -5,17 +5,25 @@ sudo modprobe kvm-intel
 
 killall vde_switch
 
+clear
+
 echo "starting switch"
-vde_switch -mod 770 -tap0 &
+vde_switch -mod 770 -tap 0 &
 
-qemu-system-x86_64 -enable-kvm -hda alpha.img -net nic,macaddr=00:00:00:00:00:01 \
+qemu-system-x86_64 -enable-kvm -net nic,macaddr=00:00:00:00:00:01 \
+	-net vde,vlan=0,sock=/var/run/vde.ctl  \
+  -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0" charlie.img &
+
+sleep 5s
+wait
+exit
+
+qemu-system-x86_64 -enable-kvm -net nic,macaddr=00:00:00:00:00:02 \
 	-net vde,vlan=0,sock=/var/run/vde.ctl \
-  -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0" &
+  -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0" bravo.img &
 
-qemu-system-x86_64 -enable-kvm -hda bravo.img -net nic,macaddr=00:00:00:00:00:02 \
-	-net vde,vlan=0,sock=/var/run/vde.ctl  \
-  -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0" &
+wait
 
-qemu-system-x86_64 -enable-kvm -hda charlie.img -net nic,macaddr=00:00:00:00:00:03 \
-	-net vde,vlan=0,sock=/var/run/vde.ctl  \
-  -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0"
+# qemu-system-x86_64 -enable-kvm -hda charlie.img -net nic,macaddr=00:00:00:00:00:03 \
+# 	-net vde,vlan=0,sock=/var/run/vde.ctl vga=791  \
+#   -kernel /usr/src/build-net-next/arch/x86/boot/bzImage -append "root=/dev/sda console=ttyS0"
